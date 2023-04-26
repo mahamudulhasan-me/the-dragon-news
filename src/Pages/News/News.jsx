03a02b82239995074e4744b206bf2bc248b3a3ef
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-04-26 22:04:39 +0600

    add details info with rating

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const News = () => {
-  return <div>This si single news</div>;
+  const { title, details, image_url } = useLoaderData();
+  return (
+    <div>
+      <h4>{title}</h4>
+      <img src={image_url} alt="" />
+      <p>{details}</p>
+    </div>
+  );
 };
 
 export default News;
--- src/Pages/NewsDetails/NewsDetails.jsx
+++ src/Pages/NewsDetails/NewsDetails.jsx
@@ -1,12 +1,52 @@
+import moment from "moment";
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Image } from "react-bootstrap";
+import {
+  FaEye,
+  FaRegBookmark,
+  FaRegStar,
+  FaShareAlt,
+  FaStar,
+} from "react-icons/fa";
+import Rating from "react-rating";
 import { Link } from "react-router-dom";
 
 const NewsDetails = (props) => {
-  const { _id, title, details, thumbnail_url, image_url } = props.news;
+  const {
+    _id,
+    title,
+    details,
+    thumbnail_url,
+    image_url,
+    author,
+    total_view,
+    rating,
+  } = props.news;
   return (
     <Card className="mb-4">
-      <Card.Header>Featured</Card.Header>
+      <Card.Header>
+        <div className="d-flex align-items-center">
+          {author ? (
+            <div className="d-flex flex-grow-1 align-items-center">
+              <Image
+                src={author?.img}
+                roundedCircle
+                style={{ width: "40px", height: "40px" }}
+              />{" "}
+              <div>
+                <p className="mb-0">{author?.name}</p>{" "}
+                <p>{moment(author?.published_date).format("MMM D, YY")}</p>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+          <div className="d-flex">
+            <FaRegBookmark />
+            <FaShareAlt />
+          </div>
+        </div>
+      </Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Img variant="top" src={image_url} />
@@ -21,7 +61,21 @@ const NewsDetails = (props) => {
           )}
         </Card.Text>
       </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      <Card.Footer className="text-muted d-flex">
+        <div className="flex-grow-1">
+          <Rating
+            placeholderRating={rating?.number}
+            emptySymbol={<FaRegStar />}
+            readonly
+            placeholderSymbol={<FaStar className="text-warning" />}
+            fullSymbol={<FaStar />}
+          />
+        </div>
+        <div>
+          <FaEye />
+          {total_view}
+        </div>
+      </Card.Footer>
     </Card>
   );
 };
