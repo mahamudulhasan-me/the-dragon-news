0c0fba292c0c5cbcb1e26137ea03174c8a34fe41
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-04-26 15:57:50 +0600

    add right nav

--- src/Pages/Shared/RightNav.jsx
+++ src/Pages/Shared/RightNav.jsx
@@ -1,18 +1,60 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import { FaGithub, FaGoogle } from "react-icons/fa";
+import { Button, ListGroup } from "react-bootstrap";
+import {
+  FaFacebookF,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+import QZone from "./QZone";
 
 const RightNav = () => {
   return (
     <div>
-      <h5>Login With</h5>
-      <Button variant="outline-primary" className="w-100 mb-3">
-        <FaGoogle className="me-1" /> Login With Google
-      </Button>
-      <Button variant="outline-secondary" className="w-100">
-        <FaGithub className="me-1" />
-        Login With GitHub
-      </Button>
+      <div>
+        <h5>Login With</h5>
+        <Button variant="outline-primary" className="w-100 mb-3">
+          <FaGoogle className="me-1" /> Login With Google
+        </Button>
+        <Button variant="outline-secondary" className="w-100">
+          <FaGithub className="me-1" />
+          Login With GitHub
+        </Button>
+      </div>
+      <div className="mt-5">
+        <h5>Find Us On</h5>
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebookF className="socialIcon" style={{ color: "#3B599C" }} />
+            Facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter className="socialIcon" style={{ color: "#58A7DE" }} />{" "}
+            Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram
+              className="socialIcon"
+              style={{
+                color: "purple",
+              }}
+            />
+            Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone />
+      <div className="amazing-add">
+        <h3>Create an Amazing Newspaper</h3>
+        <p style={{ fontWeight: "300", marginTop: "16px" }}>
+          Discover thousands of options, easy to customize layouts, one-click to
+          import demo and much more.
+        </p>
+        <Button variant="danger" className="px-3 py-2">
+          Learn More
+        </Button>
+      </div>
     </div>
   );
 };
